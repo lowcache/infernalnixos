b733ffd34350547e6be307ecbf8712543302de8d
0x29a <drawpdeadredd@hotmail.com>
2026-04-23 03:57:35 -0500

    Create flake.nix for Asus Notebook configuration
    Add flake.nix for NixOS migration setup

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Pure NixOS Migration for Asus Notebook - CachyNixOS UI Foundation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nix-cachyos-kernel.url = "github:googleson78/nix-cachyos-kernel";
+    impermanence.url = "github:nix-community/impermanence";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    microvm.url = "github:astro/microvm.nix";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.ghost = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./nixos/configuration.nix
+        ./nixos/hardware-configuration.nix
+        inputs.lanzaboote.nixosModules.lanzaboote
+        inputs.impermanence.nixosModules.impermanence
+        inputs.nix-cachyos-kernel.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.ghost = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
