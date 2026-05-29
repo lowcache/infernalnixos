22c963bf94c2f21f7a15b15583df3b93868964ca
lowcache <drawpdeadredd@gmail.com>
2026-05-29 14:02:04 -0500

    fixed fish function(s) syntax and format

--- home/shell.nix
+++ home/shell.nix
@@ -140,8 +140,53 @@
         forgstp = "sudo systemctl stop docker-forge";
       };
       functions = {
-        extract = ''
-          function extract --description "Expand/extract archives"
+        colorhex = {
+          description = "Colorize hex color codes in input text";
+          body = ''
+                python3 -c '
+            import sys, re, os
+          
+            def hex_to_rgb(hex_str):
+                hex_str = hex_str.lstrip("#")
+                if len(hex_str) == 3:
+                    hex_str = "".join([c*2 for c in hex_str])
+                return int(hex_str[0:2], 16), int(hex_str[2:4], 16), int(hex_str[4:6], 16)
+          
+            def colorize(match):
+                hex_code = match.group(0)
+                try:
+                    r, g, b = hex_to_rgb(hex_code)
+                    luminance = (0.2126 * r + 0.7152 * g + 0.0722 * b)
+                    fg_color = "0" if luminance > 128 else "15" # black or bright white text
+                    return f"\x1b[48;2;{r};{g};{b}m\x1b[38;5;{fg_color}m {hex_code} \x1b[0m"
+                except Exception:
+                    return hex_code
+          
+            pattern = re.compile(r"#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}")
+          
+            if len(sys.argv) > 1:
+                for arg in sys.argv[1:]:
+                    if arg == "-":
+                        for line in sys.stdin:
+                            sys.stdout.write(pattern.sub(colorize, line))
+                    elif os.path.exists(arg):
+                        try:
+                            with open(arg, "r") as f:
+                                for line in f:
+                                    sys.stdout.write(pattern.sub(colorize, line))
+                        except Exception:
+                            sys.stdout.write(pattern.sub(colorize, arg) + "\n")
+                    else:
+                        sys.stdout.write(pattern.sub(colorize, arg) + "\n")
+            else:
+                for line in sys.stdin:
+                    sys.stdout.write(pattern.sub(colorize, line))
+            ' $argv
+          '';
+        };
+        extract = {
+          description = "Expand/extract archives";
+          body = ''
             for file in $argv
               if test -f "$file"
                 switch "$file"
@@ -174,24 +219,21 @@
                 echo "'$file' is not a valid file"
               end
             end
-          end
-        '';
-        setwall = ''
-          function setwall --description "Set wallpaper for a specific monitor or globally"
+          '';
+        };
+        setwall = {
+          description = "Set wallpaper for a specific monitor or globally"
+            body = ''
             set -l script ~/.config/quickshell/ii/scripts/colors/switchwall.sh
-
             if test (count $argv) -eq 0
               echo "Usage: setwall <image_path> [monitor_name]"
               echo "Example: setwall ~/Pictures/wall.png DP-1"
               return 1
             end
-
-            set -l img (realpath $argv[1])
-            set -l mon $argv[2]
-
+            set - l img (realpath $argv [ 1 ])
+            set - l mon $argv [ 2 ]
             # Update the wallpaper.png symlink for matugen service
-            ln -sf "$img" ~/Pictures/wallpaper.png
-
+            ln - sf "$img" ~/Pictures/wallpaper.png
             if test -n "$mon"
               echo "Setting wallpaper for $mon..."
               $script --monitor $mon --image "$img"
@@ -199,17 +241,16 @@
               echo "Setting wallpaper globally..."
               $script --image "$img"
             end
-          end
-        '';
-        gpgkey = ''
-          function gpgkey
+          '';
+        };
+        gpgkey = {
+          description = "creates gpg keyn and displays armored output";
+          body = ''
             set -x GPG_TTY (tty)
             read -P "Enter your full name: " user_name
             read -P "Enter your email address: " user_email
             read -P "Enter a comment (optional): " user_comment
             read -S -P "Enter a passphrase: " key_passphrase
-            echo
-
             set temp_batch_file (mktemp)
             echo "%echo Generating GPG key" > $temp_batch_file
             echo "Key-Type: RSA" >> $temp_batch_file
@@ -224,27 +265,24 @@
             echo "Passphrase: $key_passphrase" >> $temp_batch_file
             echo "%commit" >> $temp_batch_file
             echo "%echo Done" >> $temp_batch_file
-
             echo "Generating GPG key..."
             gpg --batch --gen-key $temp_batch_file
-
             if test $status -ne 0
               echo "Error: GPG key generation failed."
               rm -f $temp_batch_file
               return 1
             end
-
             set key_fingerprint (gpg --list-keys --with-colons $user_email | grep '^fpr' | tail -n 1 | cut -d':' -f10)
             set safe_email (echo $user_email | tr -d ' ')
             gpg --armor --export $user_email > "./$safe_email.gpg.asc"
-
             echo "GPG key created! Key Fingerprint: $key_fingerprint"
             echo "Armored public key exported to: ./$safe_email.gpg.asc"
             rm -f $temp_batch_file
-          end
-        '';
-        rmspcs = ''
-          function rmspcs --description "Remove spaces from filenames"
+          '';
+        };
+        rmspcs = {
+          description = "Remove spaces from filenames";
+          body = ''
             set -l target_dir .
             if count $argv > /dev/null
               set target_dir $argv[1]
@@ -253,17 +291,18 @@
               echo "Error: '$target_dir' is not a directory."
               return 1
             end
+
             find $target_dir -depth -name "* *" | while read -l file
-              set -l dir (dirname "$file")
-              set -l old_name (basename "$file")
-              set -l new_name (string replace -a " " "_" "$old_name")
-              echo "Renaming: $old_name -> $new_name"
-              mv "$file" "$dir/$new_name"
-            end
-          end
-        '';
-        priv-sync = ''
-          function priv-sync --description "Safely sync live persistent data to priv.bkup"
+            set -l dir (dirname "$file")
+            set -l old_name (basename "$file")
+            set -l new_name (string replace -a " " "_" "$old_name")
+            echo "Renaming: $old_name -> $new_name"
+            mv "$file" "$dir/$new_name"
+          '';
+        };
+        priv-sync = {
+          description = "Safely sync live persistent data to priv.bkup";
+          body = ''
             set -l REPO_DIR /home/lowcache/Storage/priv.bkup
             set -l LIVE_HOME /persist/home/lowcache
 
@@ -273,10 +312,10 @@
             set -l DIRS Documents Pictures CodeRep unDevel AppImage ZAP-Sessions fonts krita-master .bin crypto-bot
 
             for dir in $DIRS
-                if test -d $LIVE_HOME/$dir
-                    echo "Syncing $dir..."
-                    rsync -au --progress $LIVE_HOME/$dir/ $REPO_DIR/$dir/
-                end
+              if test -d $LIVE_HOME/$dir
+                echo "Syncing $dir..."
+                rsync -au --progress $LIVE_HOME/$dir/ $REPO_DIR/$dir/
+              end
             end
 
             # Surgical .gemini sync
@@ -292,8 +331,8 @@
             rsync -au --progress $LIVE_HOME/.gnupg/ $REPO_DIR/.gnupg/
 
             echo "Sync complete. You can now commit changes in $REPO_DIR."
-          end
-        '';
+          '';
+        };
       };
     };
     git = {
@@ -348,4 +387,5 @@
   };
   services.ssh-agent.enable = true;
 }
+
 
